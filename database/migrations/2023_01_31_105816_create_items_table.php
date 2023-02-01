0555db727d93f908a1632d2f4c68d791f55f5821
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-01 11:10:06 +0545

    Created Items model

--- database/migrations/2023_01_31_105816_create_items_table.php
+++ database/migrations/2023_01_31_105816_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('Items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('cat_id');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('Items');
     }
 };
