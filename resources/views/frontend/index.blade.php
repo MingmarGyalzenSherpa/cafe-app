09f88082f015abb630ee4dccd2a864c04d296ff4
aayushmandavya <mandavyaaayush98@gmail.com>
2023-02-16 21:43:54 +0545

    frontend 54.5percent

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -296,7 +296,7 @@
               </div><!-- Menu Item -->
 
               <div class="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-2.png" class="glightbox"><img src="assets/img/menu/menu-item-2.png" class="menu-img img-fluid" alt=""></a>
+                <a href="{{asset('import/assets/img/menu/menu-item-2.png')}}" class="glightbox"><img src="{{asset('import/assets/img/menu/menu-item-2.png')}}" class="menu-img img-fluid" alt=""></a>
                 <h4>Aut Luia</h4>
                 <p class="ingredients">
                   Lorem, deren, trataro, filede, nerada
