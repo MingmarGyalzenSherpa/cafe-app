af9ce90ada31e56a30f91b53b46756a3e43c200e
SandeepPanday2501 <pandayshiroyasha@gmail.com>
2023-02-18 21:12:11 +0545

    front14

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -463,23 +463,23 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-3.png" class="glightbox"><img src="assets/img/menu/menu-item-3.png" class="menu-img img-fluid" alt=""></a>
-                <h4>Est Eligendi</h4>
+                <h4>Chicken Fried Rice</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+            Cooked white rice, chicken breasts.
                 </p>
                 <p class="price">
-                  $8.95
+                  Rs.400
                 </p>
               </div><!-- Menu Item -->
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-4.png" class="glightbox"><img src="assets/img/menu/menu-item-4.png" class="menu-img img-fluid" alt=""></a>
-                <h4>Eos Luibusdam</h4>
+                <h4>Thai Fried Rice</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                  Cooked white rice,chicken breasts, thinly sliced,Kosher salt.
                 </p>
                 <p class="price">
-                  $12.95
+                  Rs.450
                 </p>
               </div><!-- Menu Item -->
 
