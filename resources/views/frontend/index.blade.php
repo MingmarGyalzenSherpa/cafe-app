10728a37e510d064b62467a2bbe837ddfebf7780
SandeepPanday2501 <pandayshiroyasha@gmail.com>
2023-02-18 21:26:16 +0545

    front20

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -396,12 +396,12 @@
 
               <div class="col-lg-4 menu-item">
                 <a href="assets/img/menu/menu-item-4.png" class="glightbox"><img src="assets/img/menu/menu-item-4.png" class="menu-img img-fluid" alt=""></a>
-                <h4>Eos Luibusdam</h4>
+                <h4>Chicken Patty</h4>
                 <p class="ingredients">
-                  Lorem, deren, trataro, filede, nerada
+                  smoked paprika,Sliced beefsteak tomatoes.
                 </p>
                 <p class="price">
-                  $12.95
+                  Rs.250
                 </p>
               </div><!-- Menu Item -->
 
