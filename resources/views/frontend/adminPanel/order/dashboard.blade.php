a5d52436485b66d7789bdddfe9f7c12a2cf127df
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-21 21:24:07 +0545

    basic structure for rendering recipe made

--- resources/views/frontend/adminPanel/order/dashboard.blade.php
+++ resources/views/frontend/adminPanel/order/dashboard.blade.php
@@ -1,15 +1,57 @@
 @extends('frontend.adminPanel.order.layouts.main')
 @section('container')
 <div class="container " style="margin-top:90px;border:1px solid red;">
-    <nav class="navbar border">
-            <div class="nav">
+    
+            <nav class="nav border">
               <a class="nav-item border active p-3 " aria-current="page" href="#">Home</a>
               <a class="nav-item p-3 border" href="#">Features</a>
               <a class="nav-item border p-3" href="#">Pricing</a>
               <a class="nav-item border p-3" href="#">Disabled</a>
+            </nav>
+      
+      <div class="mt-3 mb-3 p-2 d-flex flex-wrap container border border-primary">
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-      </nav>
-      <div class="mt-3 mb-3 p-3 container border border-primary">
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
         
       </div>
 </div>
