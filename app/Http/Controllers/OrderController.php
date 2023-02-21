b3d62774826460cc0650890e3dcaafa27cbc5275
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-21 21:56:55 +0545

    retrieve and render categories from db done

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
     //
     public function create()
     {
-        return view('frontend.adminPanel.order.dashboard');
+        $categories = DB::table('categories')->get();
+        $items = DB::table('items')->get();
+        return view('frontend.adminPanel.order.dashboard', ['categories' => $categories, 'items' => $items]);
     }
 }
--- resources/views/frontend/adminPanel/order/dashboard.blade.php
+++ resources/views/frontend/adminPanel/order/dashboard.blade.php
@@ -3,49 +3,18 @@
 <div class="container " style="margin-top:90px;border:1px solid red;">
     
             <nav class="nav border">
-              <a class="nav-item border active p-3 " aria-current="page" href="#">Home</a>
-              <a class="nav-item p-3 border" href="#">Features</a>
-              <a class="nav-item border p-3" href="#">Pricing</a>
-              <a class="nav-item border p-3" href="#">Disabled</a>
+              <a class="nav-item border active p-3" style="width:100px;text-align:center;" aria-current="page" href="#">ALL</a>
+              <a class="nav-item p-3 border" style="width:100px;text-align:center;"  href="#">Features</a>
+              <a class="nav-item border p-3" style="width:100px; text-align:center;"  href="#">Pricing</a>
+              <a class="nav-item border p-3" style="width:100px;text-align:center;"  href="#">Disabled</a>
+              @foreach ($categories as $category)
+                <a class="nav-item border p-3" style="min-width:100px;text-align:center;"  href="#">{{$category->cat_name}}</a>
+              @endforeach
+             
+              
             </nav>
       
       <div class="mt-3 mb-3 p-2 d-flex flex-wrap container border border-primary">
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
         <div class="card m-2" style="width: 18rem;">
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
