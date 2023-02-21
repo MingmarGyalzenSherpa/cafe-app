534f8ab8df2d79b7310ecb00d2900a017bdd1396
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-21 22:05:03 +0545

    retrieve and render item from db done

--- resources/views/frontend/adminPanel/order/dashboard.blade.php
+++ resources/views/frontend/adminPanel/order/dashboard.blade.php
@@ -21,7 +21,17 @@
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
+        @foreach($items as $item)
         
+          
+            <div class="card m-2" style="width: 18rem;">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h3>{{$item->name}}</h3>
+              </div>
+          </div>
+
+        @endforeach
       </div>
 </div>
 @endsection
