e6b8882da7c89d39dd78b0b8e0f0f0c0369eed6e
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-23 18:42:33 +0545

    created billing view

---
+++ resources/views/frontend/adminPanel/cashier/bill.blade.php
@@ -0,0 +1,7 @@
+@extends('frontend.adminPanel.layouts.main');
+@section('container')
+
+    <div class="container" style="margin-top:90px;">
+        <h1>This is billing</h1>
+    </div>
+@endsection
