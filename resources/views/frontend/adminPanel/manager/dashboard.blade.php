bd7152da58afc45e4216b8c23d8a1b0ed4f7b690
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-02-24 14:04:52 +0545

    basic skeleton form to add item

--- resources/views/frontend/adminPanel/manager/dashboard.blade.php
+++ resources/views/frontend/adminPanel/manager/dashboard.blade.php
@@ -1,6 +1,31 @@
 @extends('frontend.adminPanel.layouts.main');
 @section('container')
     <div class="container" style="margin-top:90px;">
-        <h1>This is Manager</h1>
+        <h1>Items</h1>
+        <form action="{{}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <label for="name">Item Name:</label>
+                <input type="text" name="name" >
+                <br>
+                <br>
+            <label for="category">Category</label>
+            <select name="category" id="">
+                <option value="hi">food</option>
+                <option value="hi">foods</option>
+                <option value="hi">fooed</option>
+            </select>
+            <br>
+            <br>
+            <label for="image">Image:</label>
+            <input type="file" name="img">
+            <br>
+            <br>
+            <label for="price">price:</label>
+            <input type="text" name="price">
+            <br>
+            <button type="submit">Add Item</button>
+        </form>    
+        
+        
     </div>
 @endsection
